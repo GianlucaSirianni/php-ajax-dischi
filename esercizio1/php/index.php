f9ad83dcf88bc0bd8e745a47a806beb41b659aec
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-12-15 15:35:10 +0100

    esercizio 1 completo

---
+++ esercizio1/php/index.php
@@ -0,0 +1,144 @@
+<?php
+
+$database = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => 2000,
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ]
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Title</title>
+<!-- BOOSTRAP CSS LINK-->
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
+<!-- FONTAWESOME LINK-->
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer'>
+<!-- CSS LINK-->
+<link rel='stylesheet' href='./style.css'>
+</head>
+
+<body>
+
+<style>
+
+.row{
+    gap: 15px;
+    justify-content: space-between;
+}
+
+.card{
+    width: calc(100%/6
+    );
+    padding: 20px;
+}
+
+</style>
+
+
+<div class="container">
+    <div class="row" style="gap: 10px;">
+
+            <?php foreach ($database as $data) { ?>
+
+    <div class="card" style="width: 18rem;">
+       <img src= "<?php echo $data['poster'] ?>" class="card-img-top" alt="...">
+
+        <div class="card-body">
+           <h3 class="card-title"><?php echo $data['title'] ?></h3> 
+            <h5 class="card-title">
+                <?php echo $data['author'] ?>
+            </h5>
+            <p class="card-text">
+                <?php echo $data['year'] ?>
+            </p>
+        </div>
+    </div>
+
+
+    <?php } ?>
+    </div>
+</div>
+
+<!-- BOOTSTRAP JAVASCRIPT LINK-->
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3' crossorigin='anonymous'></script>
+
+<!-- JAVASCRIPT LINK-->
+<script type='text/javascript' src='./main.js'></script>
+</body>
+
+</html>
